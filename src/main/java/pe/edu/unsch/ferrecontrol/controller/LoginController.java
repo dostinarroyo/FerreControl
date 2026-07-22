9e15d2ec1d07ba1d14f61bbf78aaf2eee2e4fb7f
dostinarroyo <dostin.arroyo.27@unsch.edu.pe>
2026-07-22 02:09:16 -0500

    Agregué módulo de perfil y mejoras

--- src/main/java/pe/edu/unsch/ferrecontrol/controller/LoginController.java
+++ src/main/java/pe/edu/unsch/ferrecontrol/controller/LoginController.java
@@ -1,7 +1,6 @@
 package pe.edu.unsch.ferrecontrol.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
